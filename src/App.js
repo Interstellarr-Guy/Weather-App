87a68f5bc6ab70f0be81a8d0c46bd5a2a6f4fee0
Interstellar-Guy <interstellarguy29@gmail.com>
2026-02-03 13:23:39 +0530

    first m

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { getWeatherByCity } from "./api/weather";
+import "./App.css";
 
 function App() {
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState(null);
+  const [error, setError] = useState("");
+  
+  console.log("City:", city);
+
+  const handleSearch = async () => {
+    if (!city.trim()) return alert ("Enter a city name");
+
+    try {
+      setError("");
+      const data = await getWeatherByCity(city);
+      setWeather(data);
+    } catch (err) {
+      setError("Could not fetch weather, check city name or API key.");
+      setWeather(null);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Weather App</h1>
+
+      <input
+        type="text"
+        placeholder="Enter city"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+      />
+      <button onClick={handleSearch}>Get Weather</button>
+
+      {error && <p className="error">{error}</p>}
+
+      {weather && (
+        <div className="weather-info">
+          <h2>{weather.location.name}, {weather.location.country}</h2>
+          <p>🌡 Temperature: {weather.current.temp_c}°C</p>
+          <p>☁ Condition: {weather.current.condition.text}</p>
+          <img src={weather.current.condition.icon} alt="weather icon" />
+
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/api/weather.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+
+console.log("API .KEY:", process.env.REACT_APP_WEATHER_API_KEY);
+
+// Endpoint 
+const BASE_URL = "https://api.weatherapi.com/v1/current.json";
+
+export const getWeatherByCity = async (city) => {
+  try {
+    const response = await axios.get(BASE_URL, {
+      params: {
+        q: city,
+        key: process.env.REACT_APP_WEATHER_API_KEY ,
+        units: "metric",
+        aqi:"no"  // "metric" for °C
+      },
+    });
+
+    console.log("FULL RESPONSE:", response.data); // debug
+    
+    return response.data;
+  } catch (error) {
+    console.error("Weather API error:", error);
+    throw error;
+  }
+  
+
+};
